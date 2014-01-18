fe33c01687cb5d52cbbf4ad70b2c6ca00f7ad880
Ludovic Borie <l.borie@kanithael.net>
2014-01-18 01:06:56 +0100

    Premier commit, initialisation du site

---
+++ js/config.js
@@ -0,0 +1,17 @@
+boomerang.factory('Config', function () {
+    return {
+        //modify these
+        'name'          : 'GDG Lille',
+        'id'            : '108064444359793252329',
+        'google_api'    : 'AIzaSyB254p67GeVbcvUmjYYw_A0THJSQo7EBP4',
+        'pwa_id'        : 'c2123bonrl56rs900r1m9b5rog0', //picasa web album id, must belong to google+ id above
+        'cover' : {
+            title : 'GDG Lille',
+            subtitle : 'Rejoignez-nous dans la communauté des développeurs Google !',
+            button : {
+                text : 'Inscription',
+                url : 'www.meetup.com/GDG-Lille/'
+            }
+        }
+    };
+});
