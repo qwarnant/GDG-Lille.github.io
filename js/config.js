299c8f31aebac5bb0591fe72212b103ef7c74bf8
Ludovic Borie <l.borie@kanithael.net>
2014-01-18 01:20:36 +0100

    Deuxième version, sans les photos

--- js/config.js
+++ js/config.js
@@ -6,11 +6,11 @@ boomerang.factory('Config', function () {
         'google_api'    : 'AIzaSyB254p67GeVbcvUmjYYw_A0THJSQo7EBP4',
         'pwa_id'        : 'c2123bonrl56rs900r1m9b5rog0', //picasa web album id, must belong to google+ id above
         'cover' : {
-            title : 'GDG Lille',
-            subtitle : 'Rejoignez-nous dans la communauté des développeurs Google !',
+            title : 'Rejoignez-nous !',
+            subtitle : 'La communauté autour des technologies Google !!',
             button : {
                 text : 'Inscription',
-                url : 'www.meetup.com/GDG-Lille/'
+                url : 'http://www.meetup.com/GDG-Lille/'
             }
         }
     };
