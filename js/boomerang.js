fb47822cf2ec0e44bb6a7c21a47cb8dc8a7db319
Ludovic Borie <l.borie@gmail.com>
2014-10-25 19:38:18 +0200

    New Version for test

--- js/boomerang.js
+++ js/boomerang.js
@@ -1,5 +1,8 @@
-var boomerang = angular.module('gdgBoomerang', ['ngSanitize', 'ui.bootstrap'])
-    .config(function ($routeProvider) {
+var boomerang = angular.module('gdgBoomerang', ['ngSanitize', 'ngRoute', 'ui.bootstrap'])
+    .config(function ($routeProvider, $locationProvider) {
+
+        $locationProvider.hashPrefix('!');
+
         $routeProvider.
             when("/about", {templateUrl: 'views/about.html', controller: "AboutControl"}).
             when("/news", {templateUrl: 'views/news.html', controller: "NewsControl"}).
@@ -8,65 +11,77 @@ var boomerang = angular.module('gdgBoomerang', ['ngSanitize', 'ui.bootstrap'])
             otherwise({ redirectTo: '/about' });
     });
 
-boomerang.controller('MainControl', function ($scope, Config) {
+boomerang.controller('MainControl', function ($rootScope, $scope, Config) {
     $scope.chapter_name = Config.name;
     $scope.google_plus_link = 'https://plus.google.com/' + Config.id;
     $scope.isNavCollapsed = true;
+    $rootScope.canonical = Config.domain;
 });
 
-boomerang.controller('AboutControl', function ($scope, $http, $location, Config) {
+boomerang.controller('AboutControl', function ($scope, $http, $location, $sce, Config) {
     $scope.loading = true;
     $scope.$parent.activeTab = "about";
     $scope.cover = Config.cover;
     $http.jsonp('https://www.googleapis.com/plus/v1/people/' + Config.id +
             '?callback=JSON_CALLBACK&fields=aboutMe%2Ccover%2Cimage%2CplusOneCount&key=' + Config.google_api).
         success(function (data) {
-            console.log(data);
             $scope.desc = data.aboutMe;
+            $sce.trustAsHtml($scope.desc);
+
             if (data.cover && data.cover.coverPhoto.url) {
                 $scope.cover.url = data.cover.coverPhoto.url;
             }
             $scope.loading = false;
+        })
+        .error(function (data) {
+            $scope.desc = "Sorry, we failed to retrieve the About text from the Google+ API.";
+            $scope.loading = false;
         });
 });
 
-boomerang.controller("NewsControl", function ($scope, $http, $timeout, Config) {
+boomerang.controller("NewsControl", function ($scope, $http, $timeout, $filter, $sce, Config) {
     $scope.loading = true;
     $scope.$parent.activeTab = "news";
-    $http.
-        jsonp('https://www.googleapis.com/plus/v1/people/' + Config.id +
-            '/activities/public?callback=JSON_CALLBACK&maxResults=10&key=' + Config.google_api).
-        success(function (response) {
-            var entries = [], i, j;
+    $http.jsonp('https://www.googleapis.com/plus/v1/people/' + Config.id +
+        '/activities/public?callback=JSON_CALLBACK&maxResults=20&key=' + Config.google_api)
+        .success(function (response) {
+            var entries = [], i, j, k;
+            var item, actor, object, itemTitle, html, thumbnails, attachments, attachment;
+            var upper, published, actorImage, entry;
+
             for (i = 0; i < response.items.length; i++) {
-                var item = response.items[i];
-                var actor = item.actor || {};
-                var object = item.object || {};
-                // Normalize tweet to a FriendFeed-like entry.
-                var itemTitle = '<b>' + item.title + '</b>';
+                item = response.items[i];
+                actor = item.actor || {};
+                object = item.object || {};
+                itemTitle = object.content;
+                published = $filter('date')(new Date(item.published), 'fullDate');
+                html = ['<p style="font-size:14px;">' + published + '</p>'];
 
-                var html = [itemTitle.replace(new RegExp('\n', 'g'), '<br />')];
-                //html.push(' <b>Read More &raquo;</a>');
+                if(item.annotation) {
+                    itemTitle = item.annotation;
+                }
 
-                var thumbnails = [];
+                html.push(itemTitle.replace(new RegExp('\n', 'g'), '<br />').replace('<br><br>', '<br />'));
 
-                var attachments = object.attachments || [];
+                thumbnails = [];
+                attachments = object.attachments || [];
+
                 for (j = 0; j < attachments.length; j++) {
-                    var attachment = attachments[j];
+                    attachment = attachments[j];
                     switch (attachment.objectType) {
                         case 'album':
-                            break;// TODO needs more work
-                            var upper = attachment.thumbnails.length > 7 ? 7 : attachment.thumbnails.length;
+                            upper = attachment.thumbnails.length > 10 ? 10 : attachment.thumbnails.length;
                             html.push('<ul class="thumbnails">');
-                            for (var k = 1; k < upper; k++) {
-                                html.push('<li class="span2"><img src="' + attachment.thumbnails[k].image.url + '" /></li>');
+                            for (k = 0; k < upper; k++) {
+                                html.push('<li class="span2"><a href="' + attachment.thumbnails[k].url + '" target="_blank">' +
+                                    '<img src="' + attachment.thumbnails[k].image.url + '" /></a></li>');
                             }
                             html.push('</ul>');
                             break;
                         case 'photo':
                             thumbnails.push({
                                 url: attachment.image.url,
-                                link: attachment.fullImage.url
+                                link: attachment.url
                             });
                             break;
 
@@ -78,17 +93,13 @@ boomerang.controller("NewsControl", function ($scope, $http, $timeout, Config) {
                             break;
 
                         case 'article':
+                        case 'event':
                             html.push('<div class="link-attachment"><a href="' +
-                                attachment.url + '">' + attachment.displayName + '</a>');
+                                attachment.url + '" target="_blank">' + attachment.displayName + '</a>');
                             if (attachment.content) {
-                                html.push('<br>' + attachment.content + '');
+                                html.push('<br>' + attachment.content);
                             }
                             html.push('</div>');
-                            break;
-                        case 'event':
-                            console.log(attachment);
-                            html.push('<b>' + attachment.displayName + '</b>');
-                            html.push('<p>' + attachment.content.replace(new RegExp('\n', 'g'), '<br />') + '</p>');
                             break;
                         default :
                             console.log(attachment.objectType);
@@ -96,11 +107,12 @@ boomerang.controller("NewsControl", function ($scope, $http, $timeout, Config) {
                 }
 
                 html = html.join('');
+                $sce.trustAsHtml(html);
 
-                var actorImage = actor.image.url;
+                actorImage = actor.image.url;
                 actorImage = actorImage.substr(0, actorImage.length - 2) + '16';
 
-                var entry = {
+                entry = {
                     via: {
                         name: 'Google+',
                         url: item.url
@@ -121,33 +133,39 @@ boomerang.controller("NewsControl", function ($scope, $http, $timeout, Config) {
                 gapi.plusone.go();
             });
             $scope.loading = false;
+        })
+        .error(function (response) {
+            $scope.desc = "Sorry, we failed to retrieve the News from the Google+ API.";
+            $scope.loading = false;
+            $scope.status = 'ready';
         });
-
 });
 
 boomerang.controller("EventsControl", function ($scope, $http, Config) {
     $scope.loading = true;
     $scope.$parent.activeTab = "events";
 
-    $scope.events = {past: [], future: []};
-    $http.get("http://gdgfresno.com/gdgfeed.php?id=" + Config.id).
-        success(function (data) {
-            var now = new Date();
-            for (var i = data.length - 1; i >= 0; i--) {
-                var start = new Date(data[i].start);
-
-                data[i].start = start;
-                data[i].end = new Date(data[i].end);
-
-                if (start < now) {
-                    $scope.events.past.push(data[i]);
-                } else {
-                    $scope.events.future.push(data[i]);
-                }
+    $scope.events = {past:[] ,future:[]};
+    var url = 'http://hub.gdgx.io/api/v1/chapters/' + Config.id + '/events/upcoming?callback=JSON_CALLBACK';
+    var headers = { 'headers': {'Accept': 'application/json;'}, 'timeout': 2000 };
+    $http.jsonp(url, headers)
+        .success(function (data) {
+            for (var i = data.items.length - 1; i >= 0; i--) {
+                $scope.events.future.push(data.items[i]);
             }
             $scope.loading = false;
+            $scope.status = 'ready';
         });
 
+    url = 'http://hub.gdgx.io/api/v1/chapters/' + Config.id + '/events/past?callback=JSON_CALLBACK';
+    $http.jsonp(url, headers)
+        .success(function (data) {
+            for (var i = data.items.length - 1; i >= 0; i--) {
+                $scope.events.past.push(data.items[i]);
+            }
+            $scope.loading = false;
+            $scope.status = 'ready';
+        });
 });
 
 boomerang.controller("PhotosControl", function ($scope, $http, Config) {
@@ -156,11 +174,11 @@ boomerang.controller("PhotosControl", function ($scope, $http, Config) {
     $scope.photos = [];
 
     var pwa = 'https://picasaweb.google.com/data/feed/api/user/' + Config.id + '/albumid/' + Config.pwa_id +
-        '?access=public&alt=json-in-script&kind=photo&max-results=20&fields=entry(title,link/@href,summary,content/@src)&v=2.0&callback=JSON_CALLBACK';
+        '?access=public&alt=json-in-script&kind=photo&max-results=50&fields=entry(title,link/@href,summary,content/@src)&v=2.0&callback=JSON_CALLBACK';
 
     $http.jsonp(pwa).
-        success(function (d) {
-            var p = d.feed.entry;
+        success(function (data) {
+            var p = data.feed.entry;
             for (var x in p) {
                 var photo = {
                     link: p[x].link[1].href,
@@ -170,6 +188,10 @@ boomerang.controller("PhotosControl", function ($scope, $http, Config) {
                 };
                 $scope.photos.push(photo);
             }
+            $scope.loading = false;
+        })
+        .error(function (data) {
+            $scope.error_msg = "Sorry, we failed to retrieve the Photos from the Picasa Web Albums API. Logging out of your Google Account and logging back in may resolve this issue.";
             $scope.loading = false;
         });
 });
--- js/config.js
+++ js/config.js
@@ -3,14 +3,15 @@ boomerang.factory('Config', function () {
         //modify these
         'name'          : 'GDG Lille',
         'id'            : '108064444359793252329',
-        'google_api'    : 'AIzaSyB254p67GeVbcvUmjYYw_A0THJSQo7EBP4',
-        'pwa_id'        : 'c2123bonrl56rs900r1m9b5rog0', //picasa web album id, must belong to google+ id above
+        'google_api'    : 'AIzaSyA9ALjr2iWvhf3Rsz9-bH0cEcDcrdkpuAg',
+        'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
+        'domain'        : 'http://www.gdgspacecoast.org',
         'cover' : {
-            title : 'Rejoignez-nous !',
-            subtitle : 'La communauté autour des technologies Google !!',
-            button : {
-                text : 'Inscription',
-                url : 'http://www.meetup.com/GDG-Lille/'
+            title: 'Google Developer Group Lille',
+            subtitle: 'Learn. Imagine. Create. Enjoy.',
+            button: {
+                text: 'Nous rejoindre',
+                url: '#'
             }
         }
     };
